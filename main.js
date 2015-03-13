86f9bb3cae3b0515041fb8a87cd2375491139637
ArgentumSunrise <coltergiem@gmail.com>
2015-03-12 19:30:11 -0600

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
         };
         if (i > 15) {
             $('#cornertext').css('display', 'none');
-            $('#slideshow').css('padding-top','200px')
+            $('#slideshow').css('padding-top','100px').css('height','400px')
             $('#centertext').fadeIn(500);
 
         };
