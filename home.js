7d8f2ebd2fbf6b50e550a770e877a42de7395e16
Colter Giem <coltergiem@gmail.com>
2015-03-29 18:41:59 -0600

    Finished migration from main.js

--- home.js
+++ home.js
@@ -32,4 +32,21 @@ $(document).ready(function(){
             $('#home-nav').css('cursor', 'default');
         };
     });
+    
+    $('#allahu-akbar').click(function () {
+        $(this).css('color', '#ff5555');
+        setTimeout(function () {
+            $('#allah-e-egg').show(50);
+            $('#a-e-i-1').show(50).delay(4000).hide(50);
+            if (eea <= 5) {
+                setInterval(function () {
+                    eea++;
+                    $('#a-e-i-' + eea).css('z-index','100+eea').show(50).delay(3000).hide(50);
+               }, 2100);
+           }
+            setTimeout(function () {
+                $('#allah-e-egg').hide(50);
+            }, 16400);
+        }, 3000);
+    });
 })
