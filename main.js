64ff477baeb95ddff61a42dfee31b459d691816c
Colter Giem <coltergiem@gmail.com>
2015-03-29 18:26:49 -0600

    Update main.js

--- main.js
+++ main.js
@@ -58,7 +58,6 @@ $(document).ready(function () {
     var i = 0;
     var eea = 1;
     var names = ['London', 'Paris', 'Cape Town', 'Moscow', 'New York', 'Singapore', 'Rome', 'Istanbul', 'Sydney', 'Venice', 'Dubai', 'Honolulu', 'Prague', 'Taipei', 'Rio de Janeiro'];
-    var current = window.location.pathname;
 
     setInterval(function () {
         i++;
@@ -78,7 +77,6 @@ $(document).ready(function () {
         };
     }, 3250);
 
-if(current === "/home.html"||"/home"){
     $('#home-nav').click(function () {
         recursive++;
         $('#r' + recursive).slideDown(300);
@@ -89,7 +87,7 @@ if(current === "/home.html"||"/home"){
             $('#home-nav').css('cursor', 'default');
         };
     });
-}
+    
     $('#contact-nav').click(function () {
         $('#contactbar').slideDown(600);
     });
