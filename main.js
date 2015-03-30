58936728c8c1cd33d49f80e22615c76f2cd99d39
Colter Giem <coltergiem@gmail.com>
2015-03-29 18:41:25 -0600

    I mean, really hard.

--- main.js
+++ main.js
@@ -13,22 +13,4 @@ $(document).ready(function () {
     $('#contactexit').click(function () {
         $('#contactbar').slideUp(600);
     });
-
-    $('#allahu-akbar').click(function () {
-        $(this).css('color', '#ff5555');
-        setTimeout(function () {
-            $('#allah-e-egg').show(50);
-            $('#a-e-i-1').show(50).delay(4000).hide(50);
-            if (eea <= 5) {
-                setInterval(function () {
-                    eea++;
-                    $('#a-e-i-' + eea).css('z-index','100+eea').show(50).delay(3000).hide(50);
-               }, 2100);
-           }
-            setTimeout(function () {
-                $('#allah-e-egg').hide(50);
-            }, 16400);
-        }, 3000);
-    });
-
 });
