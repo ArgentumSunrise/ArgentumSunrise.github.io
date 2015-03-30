068cf9473c492caf00f40be87639a5ae0c74dde5
Colter Giem <coltergiem@gmail.com>
2015-03-29 18:31:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ angular.module('partialsApp',[])
     .config(function($routeProvider, $locationProvider){
         $locationProvider.html5Mode(true);
         $routeProvider
-            .when("/", {templateUrl:"home.html"})
+            .when("/#/", {templateUrl:"home.html"})
             .when("/about",{templateUrl:"about.html"})
     })
 
