9793e2cb9eb835ae5ede25b8a1a88e04e1c3b96c
Colter Giem <coltergiem@gmail.com>
2015-03-29 18:39:59 -0600

    Transferring...

--- main.js
+++ main.js
@@ -7,40 +7,6 @@ angular.module('partialsApp',[])
     })
 
 $(document).ready(function () {
-    var recursive = 0;
-    var i = 0;
-    var eea = 1;
-    var names = ['London', 'Paris', 'Cape Town', 'Moscow', 'New York', 'Singapore', 'Rome', 'Istanbul', 'Sydney', 'Venice', 'Dubai', 'Honolulu', 'Prague', 'Taipei', 'Rio de Janeiro'];
-
-    setInterval(function () {
-        i++;
-        if (i <= 15) {
-            $('#cornertext').text(names[i - 1]);
-            $('#p' + i).fadeIn(500);
-            $('#cornertext').fadeIn(500);
-            setTimeout(function () {
-                $('#p' + i).fadeOut(500);
-                $('#cornertext').fadeOut(500);
-            }, 2250);
-        };
-        if (i > 15) {
-            $('#cornertext').css('display', 'none');
-            $('#centertext').fadeIn(500);
-
-        };
-    }, 3250);
-
-    $('#home-nav').click(function () {
-        recursive++;
-        $('#r' + recursive).slideDown(300);
-        setTimeout(function () {
-            $('#r' + recursive).slideUp(400)
-        }, 1000);
-        if (recursive === 6) {
-            $('#home-nav').css('cursor', 'default');
-        };
-    });
-    
     $('#contact-nav').click(function () {
         $('#contactbar').slideDown(600);
     });
