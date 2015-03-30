b90b1234014d972559355610f9f4f440ccd8e601
Colter Giem <coltergiem@gmail.com>
2015-03-30 07:05:16 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var partialsApp = angular.module('partialsApp',[])
     partialsApp.config(['$routeProvider',function($routeProvider){
         $routeProvider.
-            when("/#/", {templateUrl:"home.html"}).
+            when("/", {templateUrl:"home.html"}).
             when("/about",{templateUrl:"about.html"}).
             otherwise({redirectTo:"home.html"})
     }]);
