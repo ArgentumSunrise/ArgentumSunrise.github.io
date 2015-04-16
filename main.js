dd55bb8568e23cae290a1c30a7d6ceaef6f602f6
Colter Giem <coltergiem@gmail.com>
2015-04-15 20:07:46 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var partialsApp = angular.module('partialsApp',['ngRoute'])
  
     partialsApp.config(['$routeProvider',function($routeProvider){
         $routeProvider.
-            when("/", {templateUrl:"home.html"}, controller: 'PartialsController').
-            when("/about",{templateUrl:"about.html", controller: 'PartialsController'}).
-            otherwise({redirectTo:"/", controller: 'PartialsController'})
+            when("/rising-sun/", {templateUrl:"home.html"}, controller: 'PartialsController').
+            when("/rising-sun/about",{templateUrl:"about.html", controller: 'PartialsController'}).
+            otherwise({redirectTo:"/rising-sun/", controller: 'PartialsController'})
         $locationProvider.html5(true);
     }]);
