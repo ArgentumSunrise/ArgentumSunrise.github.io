3669e3a56ddc7501bb37b9eb766f6d776b9bfd65
Colter Giem <coltergiem@gmail.com>
2015-04-16 20:29:50 -0600

    Update main.js

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ var partialsApp = angular.module('partialsApp',['ngRoute'])
  
     partialsApp.config(['$routeProvider',function($routeProvider){
         $routeProvider.
-            when("/rising-sun", {templateUrl:"home.html"}).
-            when("/rising-sun/about",{templateUrl:"about.html"}).
-            otherwise({redirectTo:"/rising-sun"})
+            when(" ArgentumSunrise.github.io/rising-sun", {templateUrl:"home.html"}).
+            when("ArgentumSunrise.github.io/rising-sun/about",{templateUrl:"about.html"}).
+            otherwise({redirectTo:"ArgentumSunrise.github.io/rising-sun"})
         $locationProvider.html5(true);
     }]);
